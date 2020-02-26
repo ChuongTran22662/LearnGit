34fab9f26e9109319ff2ea0d9de2382b4f642718
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-02-26 08:19:12 +0700

    add Cat rever

--- Cat.js
+++ Cat.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function Cat() {
     this.dead = true;
     this.stomach = [];
@@ -5,6 +6,10 @@ function Cat() {
 
 Cat.prototype.die = function () {
     this.dead = false;
+=======
+function Cat(){
+    console.log('This is a cat');
+>>>>>>> parent of 7144199... add function die for cat
 }
 
 Cat.prototype.eat = function(mouse){
