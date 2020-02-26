ab9f5aab033952e908f49f9cccfac1ba13987f23
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-02-26 07:22:57 +0700

    test restore

--- Cat.js
+++ Cat.js
@@ -1,8 +1,14 @@
 function Cat() {
     this.dead = true;
+    this.stomach = [];
 }
+
 Cat.prototype.die = function () {
     this.dead = false;
+}
+
+Cat.prototype.eat = function(mouse){
+    this.stomach.push(mouse);
 }
 
 module.exports = Cat;
